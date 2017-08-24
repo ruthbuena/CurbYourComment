bdb99df8e31f9e040a7b113fc378486fe82481a7
Ruth Buenaflor <roofio@Ruths-iMac.local>
2017-08-24 15:21:04 -0400

    Initial File creation

---
+++ brocial_db.sql
@@ -0,0 +1,14 @@
+DROP DATABASE IF EXISTS brocial_networkDB;
+CREATE database brocial_networkDB;
+
+USE brocial_networkDB;
+
+CREATE TABLE bro (
+  id INTEGER NOT NULL AUTO_INCREMENT,
+  name VARCHAR(100) NOT NULL,
+  age INTEGER(100) NULL,
+  height_inches INTEGER(100) NULL,
+  weight DECIMAL(10,1) NULL,
+  photo BLOB NULL,
+  PRIMARY KEY (id)
+);
