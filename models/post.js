c575200fee78d60ce1d105ae32d058bf21db83c5
Rapwaz Yifty <yifty08@icloud.com>
2017-08-26 05:57:39 -0400

    initial sequelize code

--- models/post.js
+++ models/post.js
@@ -1,20 +1,20 @@
 module.exports = function(sequelize, DataTypes) {
   var Post = sequelize.define("Post", {
-    title: {
+    user name: {
       type: DataTypes.STRING,
       allowNull: false,
       validate: {
         len: [1]
       }
     },
-    body: {
+    description: {
       type: DataTypes.TEXT,
       allowNull: false,
       len: [1]
     },
     category: {
       type: DataTypes.STRING,
-      defaultValue: "Personal"
+      defaultValue: "bruh"
     }
   });
   return Post;
