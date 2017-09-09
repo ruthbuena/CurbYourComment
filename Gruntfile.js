0d3ceeb1438de494401944ebebfde6d70af2fddf
ruthbuena <ruthbpeace@gmail.com>
2017-09-09 02:07:38 -0400

    addition of travis ci

--- Gruntfile.js
+++ Gruntfile.js
@@ -24,6 +24,7 @@ module.exports = function(grunt) {
 
   grunt.loadNpmTasks('grunt-sass');
   grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-travis');
 
   grunt.registerTask('default', ['sass','watch']);
 
