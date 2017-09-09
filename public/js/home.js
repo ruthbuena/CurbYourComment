abc0448279a5a06e13dde70a537102bdfe5e7146
ruthbuena <ruthbpeace@gmail.com>
2017-09-09 02:33:44 -0400

    add mp3 file to assets

--- public/js/home.js
+++ public/js/home.js
@@ -1,3 +1,20 @@
+// JavaScript function that wraps everything
+    $(document).ready(function() {
+
+      // Gets Link for Theme Song
+      var audioElement = document.createElement("audio");
+      audioElement.setAttribute("src", "assets/theme.mp3");
+
+      // Theme Button
+      $(".theme-button").on("click", function() {
+        audioElement.play();
+      });
+
+      $(".pause-button").on("click", function() {
+        audioElement.pause();
+      });
+
+
 $(document).ready(function() {
   /* global moment */
 
